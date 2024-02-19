63fc80c3fad9dbb5f523a4aa0672b86ca5fc8792
p2pahuja <p2pahuja@uwaterloo.ca>
2024-02-19 17:40:08 -0500

    add email address input

--- components/auth/SignUp.tsx
+++ components/auth/SignUp.tsx
@@ -4,9 +4,9 @@ type Props = {};
 
 const SignUp = (props: Props) => {
   return (
-    <div className="box xl:p-6 grid grid-cols-12 gap-4 xxxl:gap-6 items-center">
+    <div className="box xl:p-6 grid grid-cols-12 gap-4 xxxl:gap-6 items-center shadow-[0px_6px_30px_0px_rgba(0,0,0,0.04)]">
       <div className="col-span-12 lg:col-span-7">
-        <div className="box bg-primary/5 dark:bg-bg3 lg:p-6 xl:p-8 border border-n30 ">
+        <div className="box bg-primary/5 dark:bg-bg3 lg:p-6 xl:p-8 border border-n30 dark:border-n500">
           <h3 className="h3 mb-4">Let&apos;s Get Started!</h3>
           <p className="md:mb-6 pb-4 mb-4 md:pb-6 bb-dashed text-sm md:text-base">
             Please Enter your Email Address to Start your FinEase Application
@@ -22,7 +22,7 @@ const SignUp = (props: Props) => {
               <input
                 type="name"
                 className="w-full text-sm bg-n0 dark:bg-bg4 border border-n30 dark:border-n500 rounded-3xl px-3 md:px-6 py-2 md:py-3 mb-5"
-                placeholder="Jone"
+                placeholder="First name"
                 id="name"
                 required
               />
@@ -37,12 +37,22 @@ const SignUp = (props: Props) => {
               <input
                 type="lname"
                 className="w-full text-sm bg-n0 dark:bg-bg4 border border-n30 dark:border-n500 rounded-3xl px-3 md:px-6 py-2 md:py-3 mb-5"
-                placeholder="Doe"
+                placeholder="Second name"
                 id="lname"
                 required
               />
             </div>
           </div>
+          <label htmlFor="email" className="md:text-lg font-medium block mb-4">
+            Enter Your Email ID
+          </label>
+          <input
+            type="email"
+            className="w-full text-sm bg-n0 dark:bg-bg4 border border-n30 dark:border-n500 rounded-3xl px-3 md:px-6 py-2 md:py-3 mb-5"
+            placeholder="Enter Your Email"
+            id="email"
+            required
+          />
         </div>
       </div>
     </div>
