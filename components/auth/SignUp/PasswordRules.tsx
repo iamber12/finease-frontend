c95cba91a3c832a2115a6f149edf2a5ca4923395
p2pahuja <p2pahuja@uwaterloo.ca>
2024-02-19 18:39:40 -0500

    add basic password rules

--- components/auth/SignUp/PasswordRules.tsx
+++ components/auth/SignUp/PasswordRules.tsx
@@ -1,18 +1,35 @@
 import React from "react";
-import { IconX } from "@tabler/icons-react";
+import { IconX, IconCheck } from "@tabler/icons-react";
 
 type Props = {
   password: string;
 };
 
 const PasswordRules = (props: Props) => {
+  const { password } = props;
+
+  const isLower = (text: string): Array<string> | null => {
+    let lowerCaseLetters = /[a-z]/g;
+    console.log(text.match(lowerCaseLetters));
+    return text.match(lowerCaseLetters);
+  };
+
+  const isUpper = (text: string): Array<string> | null => {
+    var upperCaseLetters = /[A-Z]/g;
+    return text.match(upperCaseLetters);
+  };
+
   return (
     <div>
       <div className="box w-1/2 bg-primary/5 dark:bg-bg3 lg:p-6 xl:p-8 border border-n30 dark:border-n500 rounded-3xl">
         <h3>Password must contain the following:</h3>
         <div className="flex items-center gap-x-2">
           <div className="flex-none">
-            <IconX color="red" />
+            {isLower(password) ? (
+              <IconCheck color="green" />
+            ) : (
+              <IconX color="red" />
+            )}
           </div>
           <div className="flex-initial">
             <span id="letter" className="invalid">
@@ -23,7 +40,11 @@ const PasswordRules = (props: Props) => {
 
         <div className="flex items-center gap-x-2">
           <div className="flex-none">
-            <IconX color="red" />
+            {isUpper(password) ? (
+              <IconCheck color="green" />
+            ) : (
+              <IconX color="red" />
+            )}
           </div>
           <div className="flex-initial">
             <p id="capital" className="invalid">
