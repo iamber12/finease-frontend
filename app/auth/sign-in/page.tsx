48aecff27334fb4bd1d737021446cb31185bb21d
Ayushi <a2vashis@uwaterloo.ca>
2024-02-20 08:32:44 -0500

    updated structure

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,41 +1,7 @@
-import { useState } from "react";
+import SignIn from "@/components/auth/SignIn";
 
-const SignIn = () => {
-  const [showPass, setShowPass] = useState(false);
-  return (
-    <div>
-      <form>
-        <div>
-          <label htmlFor="email">
-            Enter Your Email ID
-          </label>
-          <input
-            type="email"
-            placeholder="Enter Your Email"
-            id="email"
-            required
-          />
-          <label
-            htmlFor="password">
-            Enter Your Password
-          </label>
-          <div>
-            <input
-              type={showPass ? "text" : "password"}
-              placeholder="Enter Your Password"
-              id="password"
-              required
-            />
-          </div>
-          <div className="mt-8 flex gap-6">
-            <button type="submit" className="btn px-5">
-              Login
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+const SignInPage = () => {
+  return <SignIn />;
 };
 
-export default SignIn;
+export default SignInPage;
