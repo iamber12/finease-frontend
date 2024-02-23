c18088327edf3bdbbcc9c04934f127b1e9a38a77
Ayushi <a2vashis@uwaterloo.ca>
2024-02-23 14:54:21 -0500

    created Testimonial component

--- components/demo/Testimonial.tsx
+++ components/demo/Testimonial.tsx
@@ -9,36 +9,52 @@ import { Swiper, SwiperSlide } from "swiper/react";
 const userReviews = [
   {
     id: 1,
-    name: "Arlene McCoy",
-    designation: "Software Developer",
-    img: "/images/user-big-1.png",
+    name: "Ganesh K.",
+    designation: "FinEase Investor",
+    img: "/images/user_test1.png",
     review:
-      "Great job on the code quality! Your attention to detail and dedication to producing clean, well-structured, and efficient code is impressive. Keep up the excellent work!",
+    "Very streamlined process. Working with FinEase was very easy and quite professional. Received great customer service as well. I recommend FinEase for both investing peer-to-peer and borrowing from other peers. A+ service. Keep up the excellent work!",
   },
   {
     id: 1,
-    name: "Cody Fisher",
-    designation: "Project manager",
-    img: "/images/user-big-2.png",
+    name: "Adrian Chow",
+    designation: "Finease Investor",
+    img: "/images/user_test2.png",
     review:
-      "Great job on the code quality! Your attention to detail and dedication to producing clean, well-structured, and efficient code is impressive. Keep up the excellent work!",
+    "Great interface. Interesting platform for Canadian investors to benefit from peer to peer lending.",
   },
   {
     id: 1,
-    name: "James Fillip",
-    designation: "Scrum Master",
-    img: "/images/user-big-3.png",
+    name: "Amy Fillip",
+    designation: "FinEase Borrower",
+    img: "/images/user_test3.png",
     review:
-      "Great job on the code quality! Your attention to detail and dedication to producing clean, well-structured, and efficient code is impressive. Keep up the excellent work!",
+    "My experience with FinEase was easy and stress free. The contacts by phone and emails were very professional. If you need a leg up financially, I would recommend looking into FinEase.",
   },
   {
     id: 1,
-    name: "John Snow",
-    designation: "UI/UX Designer",
-    img: "/images/user-big-4.png",
+    name: "Stephen N.",
+    designation: "FinEase Borrower",
+    img: "/images/user_test4.png",
     review:
-      "Great job on the code quality! Your attention to detail and dedication to producing clean, well-structured, and efficient code is impressive. Keep up the excellent work!",
+    "Very nice people, and very honest in why I was declined. Will try again in a few months to a year :(", 
   },
+  {
+    id: 1,
+    name: "Colin L.",
+    designation: "FinEase Borrower",
+    img: "/images/user_test5.png",
+    review:
+    "Great customer service. Really went out of there way to help get a great loan."
+  },
+  {
+    id: 1,
+    name: "Angela",
+    designation: "FinEase Investor",
+    img: "/images/user_test6.png",
+    review:
+    "Simple, easy. you know what you get! Always responsive to your questions! Thought this kind of investment were over complified for simple folk like me, but FinEase eliminate that barrier!"
+  }
 ];
 
 const Testimonial = () => {
@@ -52,7 +68,7 @@ const Testimonial = () => {
           </span>
           <h2 className="h2 mb-4 lg:mb-6 mt-4">What People Says</h2>
           <p className="text-sm md:text-base">
-            Some of our happy customers review regarding our Products, Qualities
+            Some of our happy customers review regarding our Qualities
             and Supports.
           </p>
         </div>
@@ -62,9 +78,6 @@ const Testimonial = () => {
             spaceBetween={12}
             loop
             modules={[Autoplay, Navigation]}
-            // autoplay={{
-            //   delay: 1200,
-            // }}
             navigation={{
               prevEl: ".prev-review",
               nextEl: ".next-review",
@@ -84,7 +97,7 @@ const Testimonial = () => {
                 <div className="shadow-[0px_6px_30px_0px_rgba(0,0,0,0.06)] bg-n0 dark:bg-bg4 p-3 rounded-xl">
                   <div className="bg-primary/5 dark:bg-bg3 rounded-xl border border-n30 dark:border-n500 p-4 md:p-6">
                     <ul className="mb-4 flex gap-2">
-                      {Array.from({ length: 5 }).map((item, index) => (
+                      {Array.from({ length: Math.random()*10 }).map((item, index) => (
                         <i
                           key={index}
                           className="las la-star text-[#FFC700] text-lg"></i>
