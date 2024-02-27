f2b406468eb7dfe9046c352d9ac69b03af5f2870
p2pahuja <p2pahuja@uwaterloo.ca>
2024-02-27 00:31:55 -0500

    minor fix

--- components/auth/SystemError.tsx
+++ components/auth/SystemError.tsx
@@ -20,7 +20,7 @@ const SystemError = () => {
           An error has occurred. If the problem persists, please contact a
           system administrator or try again later.
         </p>
-        <Link href="/" className="btn">
+        <Link href="/dashboard" className="btn">
           Back to Home
         </Link>
       </div>
