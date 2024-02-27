aa2013c8661eee5587fd2ed06dd255cf6c3855dd
p2pahuja <p2pahuja@uwaterloo.ca>
2024-02-26 19:14:10 -0500

    add post link

--- utils/constants.ts
+++ utils/constants.ts
@@ -19,3 +19,4 @@ export const years = range(1900, new Date().getFullYear() + 1, 1);
 
 export const SIGNUP_POST_LINK = "http://54.204.68.167:8000/v1/auth/register";
 export const SIGNIN_POST_LINK = "http://54.204.68.167:8000/v1/auth/login";
+export const PROPOSAL_POST_LINK = "http://54.204.68.167:8000/v1/loan/proposals";
