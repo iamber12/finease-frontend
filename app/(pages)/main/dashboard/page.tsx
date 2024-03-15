10a1dd8bd69d777213fc32aa5abb119b85f2a384
p2pahuja <p2pahuja@uwaterloo.ca>
2024-03-14 22:39:19 -0400

    add basic foundation

--- app/(pages)/main/dashboard/page.tsx
+++ app/(pages)/main/dashboard/page.tsx
@@ -1,31 +1,17 @@
 "use client";
-import IncomeExpences from "@/components/dashboard/IncomeExpences";
-import LatestTransactions from "@/components/dashboard/LatestTransactions";
-import TransactionAccount from "@/components/dashboard/TransactionAccount";
-import Statistics from "@/components/dashboard/Statistics";
-import Banner from "@/components/shared/Banner";
-import useDropdown from "@/utils/useDropdown";
+import { useAuth } from "@/components/auth/UserContext";
+import Loading from "../loading";
+import DashboardBorrow from "./DashboardBorrow";
+import DashboardLender from "./DashboardLender";
+
 const page = () => {
-  const { open, toggleOpen } = useDropdown();
-  
-  return (
-    <div>
-      <div className="flex justify-between flex-wrap items-center gap-4 mb-6 xl:mb-8">
-        <div></div>
-        <Banner open={open} toggleOpen={toggleOpen} title="" className="mb-0 lg:mb-0" />
-      </div>
-      <div className="grid grid-cols-12 gap-4 xxl:gap-6">
-        <div className="col-span-12 md:col-span-12 lg:col-span-12 flex flex-col gap-4 xxl:gap-6">
-          <Statistics />
-          <IncomeExpences />
-        </div>
-        <div className="col-span-12 md:col-span-12 lg:col-span-12 flex flex-col gap-4 xxl:gap-6">
-          <LatestTransactions open={open}/>
-          <TransactionAccount />
-        </div>
-      </div>
-    </div>
-  );
+  const { currentUser } = useAuth();
+
+  if (currentUser.primary_role === "Borrower") {
+    return <DashboardBorrow />;
+  } else {
+    return <DashboardLender />;
+  }
 };
 
 export default page;
