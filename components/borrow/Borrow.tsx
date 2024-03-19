d51db5a655febd66ba991c47bed8a10e92b37e30
Ayushi <a2vashis@uwaterloo.ca>
2024-03-19 19:37:57 -0400

    added features section

--- components/borrow/Borrow.tsx
+++ components/borrow/Borrow.tsx
@@ -7,6 +7,7 @@ import vehicle from "@/public/images/borrow/vehicle.jpg";
 import creditcard from "@/public/images/borrow/creditcard.png";
 import Working from "./Working";
 import Process from "./Process";
+import AmountSlider from "../shared/AmountSlider";
 
 const Borrow = () => {
   return <>
@@ -65,11 +66,13 @@ const Borrow = () => {
       <p className="text-sm dark:text-gray-300 text-center italic mt-2">
 Your data remains confidential and protected. Investors will have no access to your personal details.</p>
         </h3> 
+        <div>
         <div className="flex justify-center flex-wrap gap-6">
         <Link className="btn max-md:hidden" href="/auth/sign-up">
             Register
           </Link>
             </div>
+        </div>
       </div>
 
       <Working/>
@@ -96,6 +99,137 @@ Your data remains confidential and protected. Investors will have no access to y
         </div>
 
         <Process/>
+      </section>
+      
+      <div className="sm:w-3/4 lg:w-7/12 mx-auto px-2">
+          <h1 className="text-2xl sm:text-2xl min-[1650px]:text-[44px] !leading-tight font-semibold mb-4 lg:mb-6 mt-0 text-center">
+          FinEase helps you achieve your financial goals
+          </h1>
+          <p className="text-base text-center mt-2">
+A peer-to-peer unsecured personal loan offers a versatile solution for funding a range of necessities, including home renovations, unforeseen expenses, automobile repairs, healthcare bills, and significant acquisitions.</p>
+
+<p className="text-base text-center mt-2">Furthermore, a peer-to-peer personal loan can be an effective tool for debt consolidation. Specifically, goPeer loans enable you to merge various forms of debt, such as credit card and medical debts, potentially at a lower interest rate than your current credit card charges.</p>
+        </div>
+
+        <section className="bg-bookmark-white py-10">
+
+        {/* <!-- Feature #1 --> */}
+        <div className="relative">
+          <div className="container flex flex-col lg:flex-row items-center justify-center" style={{height: "300px"}}>
+            {/* <!-- Image --> */}
+            <div className="flex flex-1 justify-center -z-10 mb-10 lg:mb-0">
+              <Image
+                className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4  rounded-full"
+                src={creditcard}
+                alt=""
+                style={{height:"275px", width:"275px"}}
+              />
+            </div>
+            {/* <!-- Content --> */}
+            <div className="flex flex-1 flex-col lg:items-start">
+              <h2 className="h2 mb-4 text-center">
+              Credit card consolidation
+              </h2>
+              <p className="xl:text-base mb-8 lg:mb-10 text-sm md:text-sm max-xxxl:max-w-[600px] ">
+              
+Restructure your credit card debt with a more favorable interest rate to accelerate your path to financial freedom.FinEase simplifies your financial management by consolidating multiple payments into one and potentially saves you a significant amount in interest over time.
+              </p>
+            </div>
+          </div>
+          {/* <!-- Rounded Rectangle --> */}
+          <div
+            className="
+            hidden
+            lg:block
+            overflow-hidden
+            bg-bookmark-purple
+            rounded-r-full
+            absolute
+            h-80
+            w-2/4
+            -bottom-24
+            -left-36
+          "
+          ></div>
+        </div>
+         {/* <!-- Feature #2 --> */}
+         <div className="relative">
+          <div className="container flex flex-col lg:flex-row items-center justify-center" style={{height: "300px"}}>
+            {/* <!-- Image --> */}
+            <div className="flex flex-1 justify-center -z-10 mb-10 lg:mb-0">
+              <Image
+                className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4  rounded-full"
+                src={home3}
+                alt=""
+                style={{height:"200px", width:"200px"}}
+              />
+            </div>
+            {/* <!-- Content --> */}
+            <div className="flex flex-1 flex-col lg:items-start">
+              <h2 className="h2 mb-4 text-center">
+              Enhancing your living space
+              </h2>
+              <p className="xl:text-base mb-8 lg:mb-10 text-sm md:text-sm max-xxxl:max-w-[600px] ">
+              
+              Secure financing to kickstart your home renovation projects. This financial support enables you to enhance your living environment. Access to these funds not only helps in transforming your house into your dream home but also potentially increases its value and improves your quality of life. 
+              </p>
+            </div>
+          </div>
+          {/* <!-- Rounded Rectangle --> */}
+          <div
+            className="
+            hidden
+            lg:block
+            overflow-hidden
+            bg-bookmark-purple
+            rounded-r-full
+            absolute
+            h-80
+            w-2/4
+            -bottom-24
+            -left-36
+          "
+          ></div>
+        </div>
+        {/* <!-- Feature #3 --> */}
+        <div className="relative">
+          <div className="container flex flex-col lg:flex-row items-center justify-center" style={{height: "300px"}}>
+            {/* <!-- Image --> */}
+            <div className="flex flex-1 justify-center -z-10 mb-10 lg:mb-0">
+              <Image
+                className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4  rounded-full"
+                src={vehicle}
+                alt=""
+                style={{height:"200px", width:"200px"}}
+              />
+            </div>
+            {/* <!-- Content --> */}
+            <div className="flex flex-1 flex-col lg:items-start">
+              <h2 className="h2 mb-4 text-center">
+              Vehicle maintenance and loan options
+              </h2>
+              <p className="xl:text-base mb-8 lg:mb-10 text-sm md:text-sm max-xxxl:max-w-[600px] ">
+              
+ Quickly obtain financing for sudden vehicle repairs or for acquiring a new or used car. This option provides a straightforward path to manage unexpected automotive expenses or to facilitate the purchase of a vehicle, ensuring you're back on the road with minimal delay. 
+              </p>
+            </div>
+          </div>
+          {/* <!-- Rounded Rectangle --> */}
+          <div
+            className="
+            hidden
+            lg:block
+            overflow-hidden
+            bg-bookmark-purple
+            rounded-r-full
+            absolute
+            h-80
+            w-2/4
+            -bottom-24
+            -left-36
+          "
+          ></div>
+        </div>
       </section>
     </div>
     </>
