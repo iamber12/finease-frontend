684854ad62f9ba401fa7783bc348c55771f9a915
Amber Verma <a23verma@uwaterloo.ca>
2024-04-03 22:28:17 +0000

    deploy.sh edited online with Bitbucket

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ sudo systemctl stop finease-frontend.service
 cd FinEase/frontend
 git reset --hard HEAD~0
 git pull
-/home/ubuntu/.nvm/versions/node/v21.7.1/bin/npm run build
+npm run build
 sudo systemctl daemon-reload
 sudo systemctl start finease-backend.service
