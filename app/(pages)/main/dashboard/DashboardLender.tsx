f539f14fa3195e68ee18a3978a1bf95450588a82
p2pahuja <p2pahuja@uwaterloo.ca>
2024-04-04 22:45:37 -0400

    add proposal

--- app/(pages)/main/dashboard/DashboardLender.tsx
+++ app/(pages)/main/dashboard/DashboardLender.tsx
@@ -1,23 +1,34 @@
+"use client";
+import BannerProposal from "@/components/shared/BannerProposal";
 import IncomeExpenseChart from "@/components/style-03/IncomeExpenseChart";
 import LatestTransactions from "@/components/style-03/LatestTransactions";
 import Statistics from "@/components/style-03/Statistics";
 import TransactionOverview from "@/components/style-03/TransactionOverview";
+import useDropdown from "@/utils/useDropdown";
 import dynamic from "next/dynamic";
 
-const LiveUsers = dynamic(
-  () => import("@/components/style-03/Users"),
-  {
-    ssr: false,
-  }
-);
+const LiveUsers = dynamic(() => import("@/components/style-03/Users"), {
+  ssr: false,
+});
 const page = () => {
+  const { open, toggleOpen } = useDropdown();
+
   return (
-    <div className="grid grid-cols-12 gap-4 xxl:gap-6">
-      <Statistics />
-      <TransactionOverview />
-      <IncomeExpenseChart />
-      <LatestTransactions />
-      <LiveUsers />
+    <div className="flex flex-col items-end gap-2 p-2">
+      <BannerProposal
+        open={open}
+        toggleOpen={toggleOpen}
+        title=""
+        buttTitle="Quick Loan Proposal"
+        className="mb-0 lg:mb-0"
+      />
+      <div className="grid grid-cols-12 gap-4 xxl:gap-6">
+        <Statistics />
+        <TransactionOverview />
+        <IncomeExpenseChart />
+        <LatestTransactions />
+        <LiveUsers />
+      </div>
     </div>
   );
 };
